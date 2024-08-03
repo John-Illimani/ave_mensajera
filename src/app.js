f41e5ddd5bab6c86c59ae399962bd2ad34110e43
john-illimani <illimanijohn09@gmail.com>
2024-08-03 17:18:18 -0400

    version 1.3

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
 import express from "express";
 
 import UserRoutes from './routes/user.routes.js';
+import cors from "cors";
 
 const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:5173', // Reemplaza con el origen desde el cual estás haciendo las solicitudes
+    methods: 'GET,POST,PUT,DELETE',
+    credentials: true // Si necesitas enviar cookies o encabezados de autorización
+  }));
+
 
 app.use(express.json());
 
 app.use(UserRoutes);
 
 export default app;
+
+
