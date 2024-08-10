03cc6332dec4924e14410b8ab12be19e15962f73
john-illimani <illimanijohn09@gmail.com>
2024-08-10 12:52:46 -0400

    version 2.0

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ import cors from "cors";
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:5173/',  // Reemplaza con el origen desde el cual estás haciendo las solicitudes
+    origin: 'http://localhost:5173',  // Reemplaza con el origen desde el cual estás haciendo las solicitudes
     methods: 'GET,POST,PUT,DELETE',
     credentials: true // Si necesitas enviar cookies o encabezados de autorización
   }));
