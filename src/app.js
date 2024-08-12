6614e78fb27a2d54e37fd0df75870ee3f11d8f3d
john-illimani <illimanijohn09@gmail.com>
2024-08-12 09:59:03 -0400

    version 2.4

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ import cors from "cors";
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:5173',  // Reemplaza con el origen desde el cual estás haciendo las solicitudes
+    origin: 'https://john-illimani.github.io',  // Reemplaza con el origen desde el cual estás haciendo las solicitudes
     methods: 'GET,POST,PUT,DELETE',
     credentials: true // Si necesitas enviar cookies o encabezados de autorización
   }));
